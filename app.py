529d1efc68e7c2cd1ae4d5c11ccf86d52d7b1d59
Cesar Coppola Santos <cesarcoppolas@gmail.com>
2024-10-01 21:24:19 -0300

    Updated short URL output and modified Flask routes

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ import os
 from flask import Flask, redirect, render_template, url_for, request, session
 from short_urls import store_urls, get_long_url, create_json
 
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
 def ensure_https(url):
     if not url.startswith('http://') and not url.startswith('https://'):
-        url = 'https://' + url  # ou http:// dependendo do seu caso
+        url = 'https://' + url  # ou http:// dependendo do caso
     return url
 
+
 @app.route('/')
-def create():
+def home():
     return render_template('create_url.html')
 
 
@@ -27,7 +29,7 @@ def create_short_url():
     return redirect(url_for('new_url'))
 
 
-@app.route('/redirect/<short_url>')
+@app.route('/<short_url>')
 def redirect_to_long_url(short_url):
 
     long_url = get_long_url(short_url)
@@ -41,11 +43,15 @@ def redirect_to_long_url(short_url):
     
 @app.route('/new_url')
 def new_url():
+
     short_url = session.get('short_url')
     long_url = get_long_url(short_url)
     long_url = ensure_https(long_url)
-    return render_template('new_url.html', short_url=short_url, long_url=long_url)
+
+    return render_template('new_url.html', short_url=f"sh.ort/{short_url}", long_url=long_url)
+
 
 if __name__ == "__main__":
     create_json()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
+
